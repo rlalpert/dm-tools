93c57a3a13b3e8e5c288a2a9b950265d4c736815
Rachel Alpert <ambitious.fishus@gmail.com>
2018-02-13 20:23:17 -0500

    outputting to json file

--- json-converter.py
+++ json-converter.py
@@ -4,13 +4,25 @@ import pprint
 with open('5e-SRD-Monsters.json', 'r') as file:
     data = json.load(file)
 
+spellcasters = []
+
 for item in data:
     if "special_abilities" in item.keys():
         for i in item["special_abilities"]:
             if i["name"] == "Spellcasting":
+                monster = {"name": '', "spells": {}}
                 print(item["name"])
+                monster["name"] = item["name"]
                 print(pprint.pformat(i))
+                monster["spells"] = i
+                spellcasters.append(monster)
             elif i["name"] == "Innate Spellcasting":
+                monster = {"name": '', "spells": {}}
                 print(item["name"])
+                monster["name"] = item["name"]
                 print(pprint.pformat(i))
+                monster["spells"] = i
+                spellcasters.append(monster)
 
+with open('monster_spellcasters.json', 'w') as file:
+    json.dump(spellcasters, file)
